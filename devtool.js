3c59094d3e47c407c390fd6b7fd94d4df0f040e5
Erik Wetterberg <ewg@qlik.com>
2015-08-20 15:01:06 +0200

    Version 2.0
    Use floating action button
    Materialize icons
    Show handle also
    Popup for properties

--- devtool.js
+++ devtool.js
@@ -1,18 +1,27 @@
 define( ["css!./devtool.css"],
+
 	/**
 	 * @owner Erik Wetterberg (ewg)
 	 */
 	function () {
-
+		$( '<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">' ).appendTo( "head" );
 		function toggleId () {
-			var cnt = $(".devtool-tooltip").remove();
-			if(cnt.length ===0) {
+			var cnt = $( ".devtool-tooltip" ).remove();
+			if ( cnt.length === 0 ) {
 				$( '.qv-object' ).each( function ( i, el ) {
 					var s = angular.element( el ).scope();
 					if ( s.$$childHead && s.$$childHead.layout ) {
-						var layout = s.$$childHead.layout;
-						$( el ).append( "<div class='devtool-tooltip'><div>" + layout.qInfo.qId + "</div><div>" +
-						layout.visualization + "</div></div>" );
+						var layout = s.$$childHead.layout, model = s.$$childHead.model;
+						$( el ).append( '<div class="devtool-tooltip">' +
+						'<a class="devtool-btn" title="properties"><i class="small material-icons">view_list</i></a>' +
+						'<div>' + layout.qInfo.qId + ' ('+model.handle+')</div>' +
+						'<div>' + layout.visualization + '</div>' +
+						"</div>" );
+						$(el ).find('.devtool-btn' ).on('click',function(){
+							model.getProperties().then(function(reply){
+								alert(JSON.stringify(reply,null,2));
+							});
+						});
 					} else {
 						console.log( "No ID found" );
 					}
@@ -22,11 +31,12 @@ define( ["css!./devtool.css"],
 
 		return {
 			initialProperties: {
-				version: 1.0,
+				version: 2.0,
 				showTitles: false
-			},paint: function ( $element ) {
-				$element.html( "<button class='devtool-btn'>Toggle Id</button>" );
-				$element.find( "button" ).on( "click", toggleId);
+			}, paint: function ( $element ) {
+				$( ".devtool-btn" ).remove();
+				$( document.body ).append( "<button class='devtool-btn fab'><i class='material-icons'>settings</i></button>" );
+				$( ".devtool-btn" ).on( "click", toggleId );
 			}
 		};
 
