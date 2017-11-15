e97063aa763d4cc15c3d5bddde397ab4a9a783d5
Rob Wunderlich <rob@robwunderlich.com>
2017-11-15 15:49:42 -0800

    v3.2 added content menu with script & variable export functions

--- src/devtool.js
+++ src/devtool.js
@@ -1,13 +1,30 @@
-define(["./lib/jquery.modal.min", "css!./devtool.css", "css!./lib/jquery.modal.min.css"],
+var engineApp;
+
+define(["qlik",
+	"./lib/download",				// download.js module
+	"text!./devtool-context-menu.html",
+	"./lib/jquery.modal.min",
+	"css!./lib/jquery.modal.min.css",
+	"./lib/jquery.dropdown.min",
+	"css!./lib/jquery.dropdown.min.css",
+	"css!./devtool.css"
+	],
 
 	/**
 	 * @owner Erik Wetterberg (ewg)
 	 */
-	function (modal) {
+	function (qlik, download, devtoolContextMenu) {
 		$('<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">').appendTo("head");
+		$.modal.defaults = {
+			clickClose: false,
+			showClose: false
+		};
+		engineApp = qlik.currApp(this).model.engineApp;	
+
 		function toggleId() {
 			var cnt = $(".devtool-tooltip").remove();
 			if (cnt.length === 0) {
+				
 				$('.qv-object, .qv-panel-sheet').each(function (i, el) {
 					var s = angular.element(el).scope();
 					if (s.layout || (s.$$childHead && s.$$childHead.layout)) {
@@ -15,7 +32,6 @@ define(["./lib/jquery.modal.min", "css!./devtool.css", "css!./lib/jquery.modal.m
 							timing = { max: 0, latest: 0, start: 0, cnt: 0, tot: 0 };
 						$(el).append('<div class="devtool-tooltip">' +
 							'<a class="devtool-btn" title="properties"><i class="small material-icons">view_list</i></a>' +
-							//'<a class="devtool-btn" title="timing"><i class="small material-icons">timer</i></a>' +
 							'<div>' + layout.qInfo.qId + ' (' + model.handle + ')</div>' +
 							'<div>' + layout.visualization + '</div>' +
 							'<div class="devtool-timing"></div>' +
@@ -35,25 +51,7 @@ define(["./lib/jquery.modal.min", "css!./devtool.css", "css!./lib/jquery.modal.m
 						});
 						$(el).find('.devtool-btn').on('click', function () {
 							model.getProperties().then(function (reply) {
-								$('body').append(
-									'<div class="devtool-properties modal">' +
-									'<pre><div class="devtool-properties-content"></div></pre></div>'
-								).on($.modal.CLOSE, function(event, modal) {
-									$('.devtool-properties').remove();
-								});
 								$(".devtool-properties-content").html(JSON.stringify(reply, null, 2));
-																
-								// I'm using a button for copy-to-clipboard, otherwise marked text loses focus if another element type is used.
-								$('<button title = "copy to clipboard" class="devtool-btn devtool-properties-copybtn small material-icons">content_copy</button>')
-								.click( function(event) {
-									if (! window.getSelection().toString()) {	// If no user selection, then select everything
-										selectElemText($('.devtool-properties-content')[0]);
-									}
-									document.execCommand("Copy");
-									window.getSelection().removeAllRanges();
-								})
-								.prependTo(".devtool-properties");
-								
 								$('.devtool-properties').modal();								
 							});
 						});
@@ -64,15 +62,6 @@ define(["./lib/jquery.modal.min", "css!./devtool.css", "css!./lib/jquery.modal.m
 			}
 		}
 
-		// Function to select all text in an element
-		function selectElemText(elem) {
-			var range = document.createRange();
-			range.selectNodeContents(elem);
-			console.log(range.toString());
-			window.getSelection().removeAllRanges();
-			window.getSelection().addRange(range);			
-		};
-
 		return {
 			initialProperties: {
 				version: 2.0,
@@ -81,7 +70,124 @@ define(["./lib/jquery.modal.min", "css!./devtool.css", "css!./lib/jquery.modal.m
 				$(".devtool-btn").remove();
 				$(document.body).append("<button class='devtool-btn fab'><i class='material-icons'>settings</i></button>");
 				$(".devtool-btn").on("click", toggleId);
+				$(".devtool-btn").on("contextmenu", showContextMenu);
+				initModals(qlik, download, devtoolContextMenu)
+				
 			}
 		};
 
+		
+});
+
+//===== Show Context Menu =====
+function showContextMenu() {
+	//===== Show the context menu ====
+	$(".devtool-context-msg").html("");	// Clear existing message
+	$('.devtool-context-menu').modal();	
+}
+
+// Initialize the modal windows
+function initModals(qlik, download, devtoolContextMenu) {
+	// Remove existing elements
+	$(".devtool-context-menu").remove();
+	$(".devtool-properties").remove();
+	// Add modals to the DOM
+	$('body').append(devtoolContextMenu);
+	
+	// Command router
+	$("[data-devtool-command]").click(function(event) {
+		var command = $(event.currentTarget).data("devtool-command");
+		switch (command) {
+			case "Copy":
+				copyToClipboard($('.devtool-properties-content')[0]);
+				break;
+			case "ExportScript":
+				exportScript(qlik, download);
+				break;	
+			case "ExportVariables":	
+				exportVariables(qlik, download, $(event.target).data("devtool-filetype"));
+				break;	
+			default:
+			console.log("unknown option " + command );
+		}
+	});	
+}
+
+//==== Copy-to-Clipboard function ====	
+function copyToClipboard(elem) {
+	if (! window.getSelection().toString()) {	// If no user selection, then select everything
+		var range = document.createRange();
+		range.selectNodeContents(elem);
+		window.getSelection().removeAllRanges();
+		window.getSelection().addRange(range);	
+	}
+	document.execCommand("Copy");
+	window.getSelection().removeAllRanges();
+}
+
+//==== Script Export function ====			
+function exportScript(qlik, download) {
+	var filename = qlik.currApp(this).model.layout.qTitle + ".txt";	// App title will be used as filename
+	engineApp.getScript().then(function (reply) {
+		var data = 'data:text/plain;charset=utf-8,' + encodeURIComponent(reply.qScript);
+		download(data, filename, "text/plain");	// Download in the browser
+		$(".devtool-context-msg").html("Script exported to " + filename);
 	});
+}	
+
+
+//==== Variables Export function ====
+function exportVariables(qlik, download, filetype) {
+	if(filetype == "script") {
+		var str="";
+		getVariables(engineApp).then(function(vars){
+			for (var index = 0; index < vars.length; index++) {
+				var v = vars[index];
+				str += "SET " + v.qName + " = "  + v.qDefinition + ";\r\n";
+			}
+			var filename = qlik.currApp(this).model.layout.qTitle + "-variables.txt";	// App title + "-variables" will be used as filename
+			var data = 'data:text/plain;charset=utf-8,' + encodeURIComponent(str);
+			download(data, filename, "text/plain");	// Download in the browser
+			$(".devtool-context-msg").html("Variables exported to " + filename);
+		});
+	}
+	else {
+		var str="";
+		getVariables(engineApp).then(function(vars){
+			var filename = qlik.currApp(this).model.layout.qTitle + "-variables.json";	// App title + "-variables" will be used as filename
+			var data = 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(vars, null, 2));
+			download(data, filename, "application/json");	// Download in the browser
+			$(".devtool-context-msg").html("Variables exported to " + filename);
+		});
+	}
+}
+
+// Get all variables in app
+function getVariables(app) {	
+	return app.createSessionObject({
+			qVariableListDef: {
+					qType: 'variable',
+		qShowReserved: true,
+		qShowConfig: true,
+					qData: {
+							info: '/qDimInfos'
+					},
+					qMeta: {}
+			},
+			qInfo: { qId: "VariableList", qType: "VariableList" }
+	}).then(function (list) {
+			return list.getLayout().then(function (layout) {
+					return Promise.all(layout.qVariableList.qItems.map(function (d) {
+							return app.getVariableById(d.qInfo.qId).then(function (variable) {
+									return variable.getProperties().then(function(properties) {
+											if (d.qIsScriptCreated) properties.qIsScriptCreated = d.qIsScriptCreated;
+											if (d.qIsReserved) properties.qIsReserved = d.qIsReserved;
+											if (d.qIsConfig) properties.qIsConfig = d.qIsConfig;
+											
+											return properties; 
+									});
+							});
+					}));
+			});
+	});
+}
